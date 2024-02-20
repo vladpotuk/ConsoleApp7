517510123706c6f77cbe706e55dc06621f19a6c6
vladpotuk <vladpotuk01@gmail.com>
2024-02-20 19:45:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть число від 1 до 100: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number < 1 || number > 100)
+        {
+            Console.WriteLine("Помилка: введене число не знаходиться в діапазоні від 1 до 100.");
+        }
+        else
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
